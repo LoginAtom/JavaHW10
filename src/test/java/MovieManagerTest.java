bec274b5f9b73317870da14945bfb3f0853ef40d
Nata test <loginatom@gmail.com>
2025-09-04 10:41:57 +0500

    Initial commit

---
+++ src/test/java/MovieManagerTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+
+    @Test
+    public void testOne() {   //добавление фильма
+
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+
+        String[] expected = {"Film I"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testThree() {   //добавление 3-х фильмов
+
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = {"Film I", "Film II", "Film III"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLast() {   //последние фильмы
+
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = {"Film III", "Film II", "Film I"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
